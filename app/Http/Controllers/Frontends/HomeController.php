ffa22a192cdab3e8ae9956896667acd2b4ff04db
DoPhu <dophu17@gmail.com>
2020-06-24 21:26:33 +0700

    update layout index campaign

--- app/Http/Controllers/Frontends/HomeController.php
+++ app/Http/Controllers/Frontends/HomeController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers\Frontends;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use App\Http\Models\Campaign;
 
 class HomeController extends Controller
 {
     function home() {
-        return view('frontends.homes.home');
+    	$data['campaign1'] = Campaign::orderBy('created_at', 'DESC')->get();
+    	// dd($data);
+        return view('frontends.homes.home', $data);
     }
 }
